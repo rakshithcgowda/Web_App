49c130db3b62044a7420dbe987c91f437e09991e
rakshithhcgowda <rakshithgowdahc.211me240@nitk.edu.in>
2025-12-02 11:14:37 +0530

    fix

--- fix-line.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content 'src\components\LotWiseTable.tsx'
-$content[450] = "                  {tenderType === 'Goods' && evaluationMethodology !== 'Lot-wise' && ("
-$content | Set-Content 'src\components\LotWiseTable.tsx'
-
